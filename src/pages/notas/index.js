70a1f95f65bcaf9bd454b84b9f6c49b2b2dc00f9
Gabozxzz <firminogabriel89@gmail.com>
2022-10-26 17:27:41 -0300

    brunu

--- src/pages/notas/index.js
+++ src/pages/notas/index.js
@@ -1,43 +1,101 @@
-import './index.scss'
-import '../../common/index.scss'
-import { useState } from 'react'
+import { useState } from 'react';
+import './index.scss';
 
-export default function Notas(){
-    const [quantidade, setQuantidade] = useState();
-    const [respAlunos, setRespAlunos] = useState([]);
-    const [media, setMedia] = useState(0);
-    const [nota, setNota] = useState([]);
+export default function Notas() {
+  const [qtd, setQtd] = useState(0);
+  const [notasAlunos, setNotasAlunos] = useState([])
+
+  const [media, setMedia] = useState(0);
+  const [maior, setMaior] = useState(0);
+  const [menor, setMenor] = useState(0);
+
+
+  function okQtd() {
+    const x = criarArray(qtd);
+    setNotasAlunos(x);
+  }
+
+  function alterar(pos, novoValor) {
+    notasAlunos[pos] = Number(novoValor);
+    setNotasAlunos([...notasAlunos]);
+  }
+
+  function calcular() {
+    const a = calcMedia(notasAlunos);
+    const b = calcMaior(notasAlunos);
+    const c = calcMenor(notasAlunos);
+
+    setMedia(a);
+    setMaior(b);
+    setMenor(c);
+  }
+  function criarArray(tamanho) {
+    let notas = [];
+    for (let volta = 1; volta <= tamanho; volta++) {
+        notas.push(0);
+    }
+    return notas;
+}
+
+ function calcMedia(notas) {
+    let soma = 0;
+    for (let volta = 0; volta < notas.length; volta++) {
+        soma = soma + notas[volta];
+    }
+    let m = soma / notas.length;
+    return m;
+}
+
+ function calcMaior(notas) {
+    let m = 0;
+    for (let volta = 0; volta < notas.length; volta++) {
+        if (notas[volta] > m) {
+            m = notas[volta];
+        }
+    }
     
-    function gerarNovoAluno() {
-        let aluno = [];
-        for (let i = 0; i < quantidade; i++) {
-            aluno[i] = 0;
+    return m;
+}
+
+ function calcMenor(notas) {
+    let m = 11;
+    for (let volta = 0; volta < notas.length; volta++) {
+        if (notas[volta] < m) {
+            m = notas[volta];
         }
-        setRespAlunos(aluno);
     }
-    console.log(respAlunos);
+    
+    return m;
+}
+return (
+    <div className="App">
+      <header className="App-header">
+        
+        <div>
+          Qtd. Alunos: <input type='text' value={qtd} onChange={e => setQtd(e.target.value)} />
+          <button onClick={okQtd}> ok </button>
+        </div>
 
-    function CalcularMedia() {
-        let calculo = 0;
+        {notasAlunos.map((item, pos) => 
+          <div>
+            Aluno {pos+1}: <input type='text' value={notasAlunos[pos]} onChange={e => alterar(pos, e.target.value)} />
+          </div>  
+        )}
 
-        for(let i = 0; i < nota.length; i++) {
-            calculo += nota[i]
-        }
-        console.log(calculo);
 
-        setMedia(calculo / nota.length)
-    }
-
-    return(
-        <main>
-            <h1> Médias </h1>
-            <input type="text" placeholder='digite a quantidade de alunos' value={quantidade} onChange={e => setQuantidade(Number(e.target.value))} />
-            <button onClick={gerarNovoAluno}> Ok </button>
-            <div>
-                {respAlunos.map((item, posicao) => <p> Aluno: {posicao} <input type="text" value={item.nota} onChange={e => setNota(Number(e.target.value))} /> </p>)}
-            </div>
-            <button onClick={CalcularMedia}> Calcular </button>
-            <p> {media} </p>
-        </main>
-    )
+        <button onClick={calcular}> Calcular </button>
+        
+        <div>
+          Média: {media}
+        </div>
+        <div>
+          Maior: {maior}
+        </div>
+        <div>
+          Menor: {menor}
+        </div>
+        
+      </header>
+    </div>
+  );
 }
